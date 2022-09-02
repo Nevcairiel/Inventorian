f713a31817c3ca8ddb37d81a0dbe087b53bd0023
Hendrik Leppkes <h.leppkes@gmail.com>
2022-09-02 12:51:33 +0200

    Update bag filter dropdowns for 10.0

--- Bag.lua
+++ Bag.lua
@@ -12,6 +12,90 @@ Inventorian.Bag = {}
 Inventorian.Bag.pool = {}
 Inventorian.Bag.prototype = Bag
 
+local NUM_TOTAL_EQUIPPED_BAG_SLOTS = NUM_TOTAL_EQUIPPED_BAG_SLOTS or NUM_BAG_SLOTS
+
+local ContainerFrameFilterDropDown_OnLoad = ContainerFrameFilterDropDown_OnLoad
+
+if WoW10 then
+	local function OnBagFilterClicked(bagID, filterID, value)
+		C_Container.SetBagSlotFlag(bagID, filterID, value)
+		ContainerFrameSettingsManager:SetFilterFlag(bagID, filterID, value)
+	end
+
+	local function AddButtons_BagCleanup(containerFrame, level)
+		local info = UIDropDownMenu_CreateInfo()
+
+		info.text = BAG_FILTER_CLEANUP
+		info.isTitle = 1
+		info.notCheckable = 1
+		UIDropDownMenu_AddButton(info, level)
+
+		local id = containerFrame:GetBagID()
+
+		info = UIDropDownMenu_CreateInfo()
+		info.text = BAG_FILTER_IGNORE
+		info.func = function(_, _, _, value)
+			if id == -1 then -- bank
+				SetBankAutosortDisabled(not value)
+			elseif id == 0 then -- backback
+				SetBackpackAutosortDisabled(not value)
+			else
+				C_Container.SetBagSlotFlag(id, Enum.BagSlotFlags.DisableAutoSort, not value)
+			end
+		end
+
+		if id == -1 then -- bank
+			info.checked = GetBankAutosortDisabled()
+		elseif id == 0 then -- backpack
+			info.checked = GetBackpackAutosortDisabled()
+		else
+			info.checked = C_Container.GetBagSlotFlag(id, Enum.BagSlotFlags.DisableAutoSort)
+		end
+
+		UIDropDownMenu_AddButton(info, level);
+	end
+
+	local function AddButtons_BagFilters(bagID, level)
+		local info = UIDropDownMenu_CreateInfo()
+		info.text = BAG_FILTER_ASSIGN_TO
+		info.isTitle = 1
+		info.notCheckable = 1
+		UIDropDownMenu_AddButton(info, level)
+
+		info = UIDropDownMenu_CreateInfo()
+		local activeBagFilter = ContainerFrameSettingsManager:GetFilterFlag(bagID)
+
+		for i, flag in ContainerFrameUtil_EnumerateBagGearFilters() do
+			info.text = BAG_FILTER_LABELS[flag]
+			info.checked = activeBagFilter == flag
+			info.func = function(_, _, _, value)
+				return OnBagFilterClicked(bagID, flag, not value)
+			end
+
+			UIDropDownMenu_AddButton(info, level)
+		end
+	end
+
+	local ContainerFrameFilterDropDown_Initialize = function(self, level, addFiltersForAllBags)
+		local frame = self:GetParent()
+		local id = frame:GetID()
+
+		if id < 0 then
+			return
+		end
+
+		if ContainerFrame_CanContainerUseFilterMenu(id) then
+			AddButtons_BagFilters(id, level)
+		end
+
+		AddButtons_BagCleanup(frame, level)
+	end
+
+	ContainerFrameFilterDropDown_OnLoad = function(dropdown)
+		UIDropDownMenu_SetInitializeFunction(dropdown, ContainerFrameFilterDropDown_Initialize);
+	end
+end
+
 local BagID = 1
 function Inventorian.Bag:Create()
 	local item = next(self.pool)
@@ -49,8 +133,8 @@ function Inventorian.Bag:Create()
 	ht:SetAllPoints(bag)
 	bag:SetHighlightTexture(ht)
 
-	bag.filterDropDown = CreateFrame("Frame", name .. "FilterDropDown", bag, "UIDropDownMenuTemplate")
-	ContainerFrameFilterDropDown_OnLoad(bag.filterDropDown)
+	bag.FilterDropDown = CreateFrame("Frame", name .. "FilterDropDown", bag, "UIDropDownMenuTemplate")
+	ContainerFrameFilterDropDown_OnLoad(bag.FilterDropDown)
 
 	bag.FilterIcon = CreateFrame("Frame", nil, bag)
 	bag.FilterIcon:SetSize(28, 28)
@@ -115,6 +199,11 @@ function Bag:Set(parent, id)
 	end
 end
 
+-- ContainerFrameMixin compat
+function Bag:GetBagID()
+	return self:GetID()
+end
+
 function Bag:OnEvent(event, ...)
 	if event == "BANKFRAME_OPENED" or event == "BANKFRAME_CLOSED" then
 		self:Update()
@@ -145,7 +234,7 @@ function Bag:OnClick(button)
 
 	if button == "RightButton" then
 		PlaySound(SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_ON);
-		ToggleDropDownMenu(1, nil, self.filterDropDown, self, 0, 0);
+		ToggleDropDownMenu(1, nil, self.FilterDropDown, self, 0, 0);
 	elseif self:IsPurchasable() then
 		self:PurchaseSlot()
 	elseif CursorHasItem() then
@@ -239,17 +328,21 @@ function Bag:UpdateFilterIcon()
 
 	self.FilterIcon:Hide()
 	if id > 0 and not self:IsCached() then
-		for i = LE_BAG_FILTER_FLAG_EQUIPMENT, NUM_LE_BAG_FILTER_FLAGS do
-			local active = false
-			if id > NUM_BAG_SLOTS then
-				active = GetBankBagSlotFlag(id - NUM_BAG_SLOTS, i)
-			else
-				active = GetBagSlotFlag(id, i)
-			end
-			if active then
-				self.FilterIcon.Icon:SetAtlas(BAG_FILTER_ICONS[i], true)
-				self.FilterIcon:Show()
-				break
+		if ContainerFrameMixin and ContainerFrameMixin.UpdateFilterIcon then
+			ContainerFrameMixin.UpdateFilterIcon(self)
+		else
+			for i = LE_BAG_FILTER_FLAG_EQUIPMENT, NUM_LE_BAG_FILTER_FLAGS do
+				local active = false
+				if id > NUM_BAG_SLOTS then
+					active = GetBankBagSlotFlag(id - NUM_BAG_SLOTS, i)
+				else
+					active = GetBagSlotFlag(id, i)
+				end
+				if active then
+					self.FilterIcon.Icon:SetAtlas(BAG_FILTER_ICONS[i], true)
+					self.FilterIcon:Show()
+					break
+				end
 			end
 		end
 	end
